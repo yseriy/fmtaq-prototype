975a330ace656eb0a9821d5487161ccd8d422cfa
yseriy <y.seriy@hostcomm.ru>
2017-08-03 11:51:24 +0300

    Первый коммит.

---
+++ src/main/java/ys/prototype/fmtaq/FmtaqApplication.java
@@ -0,0 +1,12 @@
+package ys.prototype.fmtaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmtaqApplication.class, args);
+	}
+}
