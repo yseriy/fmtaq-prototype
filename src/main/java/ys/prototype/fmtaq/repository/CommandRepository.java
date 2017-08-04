a4a8cf0fb127d78fb1c47ce7803da9f45572eebb
yseriy <y.seriy@hostcomm.ru>
2017-08-04 18:23:20 +0300

    Очистка кода.

--- src/main/java/ys/prototype/fmtaq/repository/CommandRepository.java
+++ src/main/java/ys/prototype/fmtaq/repository/CommandRepository.java
@@ -1,9 +1,12 @@
 package ys.prototype.fmtaq.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ys.prototype.fmtaq.domain.Command;
 
 import java.util.UUID;
 
+@Repository
 public interface CommandRepository extends CrudRepository<Command, UUID> {
+
 }
