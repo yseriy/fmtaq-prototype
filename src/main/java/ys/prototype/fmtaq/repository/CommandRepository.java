fc8b3091d8e5fd28b31ad9b7239d8b504d84dc37
yseriy <y.seriy@hostcomm.ru>
2017-08-04 18:48:51 +0300

    Добавлен запрос на получение следующей команды.

--- src/main/java/ys/prototype/fmtaq/repository/CommandRepository.java
+++ src/main/java/ys/prototype/fmtaq/repository/CommandRepository.java
@@ -1,12 +1,17 @@
 package ys.prototype.fmtaq.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ys.prototype.fmtaq.domain.Command;
+import ys.prototype.fmtaq.domain.Task;
 
 import java.util.UUID;
 
 @Repository
 public interface CommandRepository extends CrudRepository<Command, UUID> {
 
+    @Query("select c from Command c where c.task = :task and c.status = ys.prototype.fmtaq.domain.CommandStatus.REGISTERED order by c.step")
+    Command getNextCommand(@Param("task") Task task);
 }
