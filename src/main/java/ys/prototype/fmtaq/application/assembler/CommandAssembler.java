c389d974ee4f710876fcb050c1b15ff09bbab192
yseriy <y.seriy@hostcomm.ru>
2017-09-18 15:42:10 +0300

    Добавлен ассемблер для Command.

---
+++ src/main/java/ys/prototype/fmtaq/application/assembler/CommandAssembler.java
@@ -0,0 +1,33 @@
+package ys.prototype.fmtaq.application.assembler;
+
+import org.springframework.stereotype.Component;
+import ys.prototype.fmtaq.domain.CommandSendService;
+import ys.prototype.fmtaq.domain.command.Command;
+import ys.prototype.fmtaq.domain.command.CommandRepository;
+
+import java.util.UUID;
+
+@Component
+public class CommandAssembler {
+
+    private final CommandSendService sendService;
+    private final CommandRepository commandRepository;
+
+    public CommandAssembler(CommandSendService sendService, CommandRepository commandRepository) {
+        this.sendService = sendService;
+        this.commandRepository = commandRepository;
+    }
+
+    public Command getById(UUID id) {
+        Command command = commandRepository.findOne(id);
+
+        if (command == null) {
+            throw new RuntimeException("cannot find command by id: " + id);
+        }
+
+        command.setSendService(sendService);
+        command.getTask().setSendService(sendService);
+
+        return command;
+    }
+}
