6a8b25b4204793042724fa0695e37405e0d58009
yseriy <y.seriy@hostcomm.ru>
2017-10-30 15:14:59 +0300

    Исправлен тест для CommandResponseJsonConverter.

--- src/test/java/ys/prototype/fmtaq/infrastructure/messaging/amqp/CommandResponseJsonConverterTest.java
+++ src/test/java/ys/prototype/fmtaq/infrastructure/messaging/amqp/CommandResponseJsonConverterTest.java
@@ -3,43 +3,37 @@ package ys.prototype.fmtaq.infrastructure.messaging.amqp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 import ys.prototype.fmtaq.application.dto.CommandResponseDTO;
-import ys.prototype.fmtaq.domain.FmtaqException;
+import ys.prototype.fmtaq.domain.CommandResponseStatus;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {CommandResponseJsonConverter.class, Jackson2ObjectMapperBuilderConfig.class})
+@SpringBootTest(classes = JacksonAutoConfiguration.class)
 public class CommandResponseJsonConverterTest {
 
     @Autowired
-    private CommandResponseJsonConverter commandResponseJsonConverter;
+    private Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
 
     @Test
-    public void toDTO() throws Exception {
-        String messageString = "{\"account\":\"bckp-tst4\"," +
-                "\"address\":\"tst100-uweb00\"," +
-                "\"id\":\"28ea0c3a-bac2-11e6-a2d2-9105eba229dc\"," +
-                "\"result\":{\"rc\":0}," +
-                "\"task_id\":\"28ea196ebac2-11e6-a2d2-9105eba229dc\"}";
-        String messageString1 = "";
-        try {
-            CommandResponseDTO commandResponseDTO = commandResponseJsonConverter.toDTO(messageString);
+    public void toDTO() {
+        String messageString = "{\"account\":\"bckp-tst4\","
+                + "\"address\":\"tst100-uweb00\","
+                + "\"id\":\"28ea0c3a-bac2-11e6-a2d2-9105eba229dc\","
+                + "\"result\":{\"rc\":0},"
+                + "\"task_id\":\"28ea196e-bac2-11e6-a2d2-9105eba229dc\"}";
 
-            assertThat(commandResponseDTO).isNotNull();
-            assertThat(commandResponseDTO.getCommandId()).isEqualTo(UUID.fromString("28ea196e-bac2-11e6-a2d2-9105eba229dc"));
+        CommandResponseJsonConverter commandResponseJsonConverter = new CommandResponseJsonConverter(jackson2ObjectMapperBuilder);
+        CommandResponseDTO commandResponseDTO = commandResponseJsonConverter.toDTO(messageString);
 
-            System.out.println(commandResponseDTO.getCommandId());
-            System.out.println(commandResponseDTO.getResponseStatus());
-            System.out.println(commandResponseDTO.getBody());
-
-        } catch (FmtaqException e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.printProperties());
-        }
+        assertThat(commandResponseDTO).isNotNull();
+        assertThat(commandResponseDTO.getCommandId()).isEqualTo(UUID.fromString("28ea196e-bac2-11e6-a2d2-9105eba229dc"));
+        assertThat(commandResponseDTO.getResponseStatus()).isEqualTo(CommandResponseStatus.OK);
     }
 }
